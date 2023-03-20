ef79e268d54e44f715204f73eda4c4dd07411320
vsathyabeece <125301268+vsathyabeece@users.noreply.github.com>
2023-03-20 18:44:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+const myTimeout1 = setTimeout(myGreeting1, 1000);
+        
+        function myGreeting1() {
+            document.getElementById("demo").innerHTML = "10";
+           
+        }
+        const myTimeout2 = setTimeout(myGreeting2, 2000);
+        
+
+        function myGreeting2() {
+           
+            document.getElementById("demo").innerHTML = "9";
+        
+                 }
+        const myTimeout3 = setTimeout(myGreeting3, 3000);
+        function myGreeting3() {
+           
+            document.getElementById("demo").innerHTML = "8";
+        
+        }
+        const myTimeout4 = setTimeout(myGreeting4, 4000);
+        function myGreeting4() {
+           
+            document.getElementById("demo").innerHTML = "7";
+        
+        }
+
+        const myTimeout5 = setTimeout(myGreeting5, 5000);
+        function myGreeting5() {
+           
+            document.getElementById("demo").innerHTML = "6";
+        
+        }
+        
+        const myTimeout6 =setTimeout(myGreeting6,6000);
+        function myGreeting6(){
+            document.getElementById("demo").innerHTML="5";
+        }
+
+        
+        const myTimeout7 =setTimeout(myGreeting7,7000);
+        function myGreeting7(){
+            document.getElementById("demo").innerHTML="4";
+        }
+        const myTimeout8 =setTimeout(myGreeting8,8000);
+        function myGreeting8(){
+            document.getElementById("demo").innerHTML="3";
+        }
+
+        const myTimeout9 =setTimeout(myGreeting9,9000);
+        function myGreeting9(){
+            document.getElementById("demo").innerHTML="2";
+        }
+
+        const myTimeout10 =setTimeout(myGreeting10,10000);
+        function myGreeting10(){
+            document.getElementById("demo").innerHTML="1";
+        }
+        const myTimeout11 =setTimeout(myGreeting11,11000);
+        function myGreeting11(){
+            document.getElementById("demo").innerHTML="Happy Independence Day";
+        }
+
